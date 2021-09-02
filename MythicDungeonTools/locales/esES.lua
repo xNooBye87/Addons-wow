588bb938a92482b2dffdc8018b952cabe08a81f5
José Manuel <89895547+xNooBye87@users.noreply.github.com>
2021-09-02 18:09:43 +0200

    Add files via upload

---
+++ MythicDungeonTools/locales/esES.lua
@@ -0,0 +1,76 @@
+if not(GetLocale() == "esES") then
+  return
+end
+local AddonName, MPL = ...;
+local L = MPL.L or {}
+
+-- Options
+L["Enable Minimap Button"] = "Activar botón del Minimapa"
+L["If the Minimap Button is enabled"] = "Botón del Minimap habilitado"
+
+-- Armor type
+L["Cloth"] = "Tela"
+L["Leather"] = "Cuero"
+L["Mail"] = "Malla"
+L["Plate"] = "Placas"
+
+-- Armor slot
+L["Head"] = "Cabeza"
+L["Neck"] = "Cuello"
+L["Shoulder"] = "Hombro"
+L["Back"] = "Espalda"
+L["Chest"] = "Torso"
+L["Wrist"] = "Muñecas"
+L["Hands"] = "Manos"
+L["Waist"] = "Cintura"
+L["Legs"] = "Piernas"
+L["Feet"] = "Pies"
+L["Finger"] = "Dedo"
+L["Trinket"] = "Abalorio"
+L["One-Hand"] = "Una mano"
+L["Off-Hand"] = "Mano izquierda"
+L["Two-Hand"] = "Dos manos"
+L["Ranged"] = "A distancia"
+
+-- Source list
+L["Dungeon Drop"] = "Drop de mazmorra"
+L["Weekly Vault"] = "Gran cámara Semanal"
+
+-- Dungeons
+L["Plaguefall"] = "Bajapeste"
+L["De Other Side"] = "El Otro Lado"
+L["Halls of Atonement"] = "Sala de la Expiación"
+L["Mists of Tirna Scithe"] = "Nieblas de Tirna Scithe"
+L["Sanguine Depths"] = "Cavernas Sanguinas"
+L["Spires of Ascension"] = "Agujas de Ascensión"
+L["The Necrotic Wake"] = "Estela Necrótica"
+L["Theater of Pain"] = "Teatro del Dolor"
+
+-- General
+L["Item Slot"] = "Tipo de item"
+L["Mythic Level"] = "Nivel de mítica"
+L["Source"] = "Source"
+L["Armor Type"] = "Tipo de armadura"
+
+-- Mythic Labels
+L["M0/Valor 1"] = "M0/Valor 1"
+L["M+2/Valor 1"] = "M+2/Valor 1"
+L["M+3/Valor 2"] = "M+3/Valor 2"
+L["M+4/Valor 3"] = "M+4/Valor 3"
+L["M+5/Valor 4"] = "M+5/Valor 4"
+L["M+6/Valor 5"] = "M+6/Valor 5"
+L["M+7/Valor 5"] = "M+7/Valor 5"
+L["M+8/Valor 6"] = "M+8/Valor 6"
+L["M+9/Valor 6"] = "M+9/Valor 6"
+L["M+10/Valor 7"] = "M+10/Valor 7"
+L["M+11/Valor 7"] = "M+11/Valor 7"
+L["M+12/Valor 8"] = "M+12/Valor 8"
+L["M+13/Valor 8"] = "M+13/Valor 8"
+L["M+14/Valor 9"] = "M+14/Valor 9"
+L["M+15/Valor 9"] = "M+15/Valor 9"
+L["Valor 10"] = "Valor 10"
+L["Valor 11"] = "Valor 11"
+L["Valor 12"] = "Valor 12"
+L["Great Vault M+12/13"] = "La gran cámara M+12/13"
+L["Great Vault M+14"] = "La gran cámara M+14"
+L["Great Vault M+15"] = "La gran cámara M+15"
